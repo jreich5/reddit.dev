ef5293d30da283b0c07dd82443ffd11f48f34c7f
Justin Reich <justinreich.dev@gmail.com>
2016-10-10 15:05:19 -0500

    Added .DS_STORE file to .gitignore and completed exercise on Laravel routes/views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/uppercase/{word}', function($word) {
+    return strtoupper($word);
 });
 
-Route::get('/sayhello/{name?}', function($name = 'Lassen') {
-    return 'Hello ' . $name;
+Route::get('/increment/{number}', function($number) {
+    return $number + 1;
+});
+
+Route::get('/add/{number1}/{number2}', function($number1, $number2) {
+    return $number1 + $number2;
+});
+
+
+// How to pass data to the view
+Route::get('/', function() {
+    $name = 'Cameron';
+    $data['name'] = $name;
+    return view('my-first-view')->with($data);
 });
